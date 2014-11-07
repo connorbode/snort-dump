511e0c6a1f06e006c1bff1c09659efdbb20832e9
Connor Bode <connor.bode@gmail.com>
2014-11-07 12:49:22 -0500

    db connection info working

--- snort-dump.js
+++ snort-dump.js
@@ -1,3 +1,11 @@
 
 // get db connection
-var db_connection = require('./sample_db_connection.js');
+var db_connection_file = process.argv[2];
+var db_connection_info;
+try {
+  db_connection_info = require(db_connection_file);
+} catch (err) {
+  console.log("Couldn't find the db connection file");
+  process.exit(0);
+}
+
