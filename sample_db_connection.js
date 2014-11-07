2c73c1256b4846419800ba180af5918897083728
Connor Bode <connor.bode@gmail.com>
2014-11-07 16:35:30 -0500

    update sample connection file

--- sample_db_connection.js
+++ sample_db_connection.js
@@ -1,8 +1,12 @@
 module.exports = {
+
+  // db connection details
   db: 'snort',
   username: 'snort',
   password: 'bacon',
   table: 'snort',
+
+  // sequelize options
   sequelize: {
     dialect: 'mysql',
     port: 3306
